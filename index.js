41622a9eafdccc8d11ea4537144a792d15256cd2
Mihirgb <mihirgajbhiye22@gmail.com>
2025-02-23 13:22:50 +0530

    add port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+const port = process.env.PORT || 4000;
 
 const app=express();
 app.get('/',(req,res)=>{
     res.send('Basic Backend')
 })
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is running on port 3000')
 })
