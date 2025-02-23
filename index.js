b05b016963673e6bb6a962f62fd35a1a36b19ee7
Mihirgb <mihirgajbhiye22@gmail.com>
2025-02-23 13:26:18 +0530

    change console.log

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/',(req,res)=>{
     res.send('Basic Backend')
 })
 app.listen(port,()=>{
-    console.log('Server is running on port 3000')
+    console.log(`Server is running on ${port}`)
 })
