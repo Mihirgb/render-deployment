983ea1c89891f5d6b4ae51d7e18d9494dafa4a14
Mihirgb <mihirgajbhiye22@gmail.com>
2025-02-23 14:17:14 +0530

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 const port = process.env.PORT || 4000;
 
 const app=express();
+app.use(cors());
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('Basic Backend')
 })
